63f6d929b8267d499c7212c4eb8baf3df4852156
nameless <gfedcba625@gmail.com>
2019-05-12 19:12:31 +0300

    added bcrypt hash for password, fixed error showing on frontend, improved backend error handling, refactoring: logic to controllers

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,11 +1,48 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const saltRounds = 10; //2-3 hashes/sec
+const passport = require('passport');
 
-exports.signUp = async (body, logIn) => {
+exports.signUp = async (request, response) => {
+    const { body } = request;
+    const salt = await bcrypt.genSalt(saltRounds);
+    body.password = await bcrypt.hash(body.password, salt);
+
     const user = await User.create(body);
-    return new Promise((reject, resolve) => {
-        logIn(user, (err) => {
-            if (err) { reject(err); }
-            resolve(user);
-        });
+
+    request.logIn(user, err => {
+        if (err) {
+            console.error(err);
+            return response
+                .status(400)
+                .send("Can't create new user");
+        }
+        response.send(201);
     });
+};
+
+exports.signIn = async (request, response, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        if (err) {
+            next(err);
+        }
+
+        if (info) {
+            // Dev only, insecure
+            const errorMessage = info.message;
+            // const errorMessage = 'Invalid username or password.';
+
+            return response
+                .status(400)
+                .send(errorMessage);
+        }
+
+        request.logIn(user, err => {
+            if (err) {
+                next(err);
+            }
+
+            response.send(200);
+        });
+    })(request, response, next);
 };
